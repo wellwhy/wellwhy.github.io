a54abe81b2b1a95b7c86946ce81681ab1d265777
wellwhy <w311why@gmail.com>
2022-11-20 14:22:45 -0500

    More

--- js/scene.js
+++ js/scene.js
@@ -12,61 +12,25 @@ var loader = new THREE.GLTFLoader();
 // scene.add(directionalLight);
 // scene.add(directionalLight.target);
 var light = new THREE.HemisphereLight( 0xcccccc, 0xffffff, 1 );
+renderer.Out
 renderer.gammaOutput = true;
 renderer.setPixelRatio( window.devicePixelRatio / 3 );
 scene.add( light );
-
-function vertexShader() {
-    return `
-      varying vec3 vUv; 
-  
-      void main() {
-        vUv = position; 
-        vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-        gl_Position = projectionMatrix * modelViewPosition; 
-      }
-    `
-}
-
-let ps1_material =  new THREE.ShaderMaterial({
-    //uniforms: uniforms,
-    //fragmentShader: fragmentShader(),
-    vertexShader: vertexShader(),
-  })
-
-// {
-//   const loader2 = new THREE.CubeTextureLoader();
-//   const texture = loader2.load([
-//     'spacecubemap3/x+.png',
-//     'spacecubemap3/x-.png',
-//     'spacecubemap3/y+.png',
-//     'spacecubemap3/y-.png',
-//     'spacecubemap3/z+.png',
-//     'spacecubemap3/z-.png',
-//   ]);
-//   scene.background = texture;
-// }
 
 camera.position.z = 2;
 var model_1;
 var mixer;
 var helper;
 var skeleton_1;
-//loader.load( 'marie/marierose2.gltf', function ( gltf ) {
 loader.load( 'loser.glb', function ( gltf ) {
     gltf.scene.traverse( function( node ) {
         if ( node.isMesh ) { 
             node.castShadow = true;
-            //node.material = ps1_material;
         }
     } );
     model_1 = gltf.scene;
     mixer = new THREE.AnimationMixer( model_1 );
     gltf.animations.forEach( ( clip, i ) => { if(i == 3) mixer.clipAction( clip ).play(); } ); //play animation at index 3
-
-    // helper = new THREE.SkeletonHelper(model_1); //Show skeleton
-    // helper.material.lineWidth = 1;
-    // helper.visible = true;
     
     // model_1.position.x = -.15;
     model_1.position.y = -1;
@@ -74,15 +38,6 @@ loader.load( 'loser.glb', function ( gltf ) {
     // model_1.position.z = -1;
     // model_1.rotation.y = -5.2;
     // model_1.rotation.x = .1;
-
-    //apparently this gives the skinned mesh from scene but gltf.scene seems to be a skinnedmesh
-    // gltf.scene.traverse( function ( child ) {
-    //     if ( child.type == 'SkinnedMesh' ) {
-    //         obj_1 = { animations: gltf.animations, mesh: child };
-    //         skeleton_1 = obj_1.mesh.skeleton;
-    //         //console.log(obj_1.mesh.skeleton.bones[10].position);
-    //     }
-    // });
 
     scene.add( model_1 ); //ORDER MATTERS HERE
     console.log(model_1);
@@ -99,49 +54,9 @@ container.appendChild( renderer.domElement );
 // var currMaxPosition = new THREE.Vector3();
 
 function animate() {
-
-    //WHY THIS SHIT DONT WORK
-    // if(getMaxBone){
-    //     if(model_1){
-    //         getMaxBone = false;
-    //         console.log(model_1.children[1].children[2].skeleton.bones);
-    //         for(var i = 0; i < model_1.children[1].children[2].skeleton.bones.length; i++){
-    //             var p = new THREE.Vector3();
-    //             var q = new THREE.Quaternion();
-    //             var s = new THREE.Vector3();
-    //             model_1.children[1].children[2].skeleton.bones[i].matrixWorld.decompose(p, q, s); //sets by reference
-    //             console.log(p);
-    //             if( currMaxPosition.y < p.y){
-    //                 maxBone = i;
-    //                 currMaxPosition.x = p.x;
-    //                 currMaxPosition.y = p.y;
-    //                 currMaxPosition.z = p.z;
-    //             }
-    //         }
-    //     }
-    //     console.log(maxBone);
-    // }
-    //console.log(obj_1.mesh.skeleton.bones[10].position);
-    //console.log("HELLO FUCKER");
-    // if(model_1){
-
-    //     //this will get the world position of the bone (bone[6].position is local or something, not sure)
-    //     var p = new THREE.Vector3();
-    //     var q = new THREE.Quaternion();
-    //     var s = new THREE.Vector3();
-    //     model_1.children[1].children[2].skeleton.bones[6].matrixWorld.decompose(p, q, s); //sets by reference
-
-    //     //you have to set position like this for some reason, you can't do camera.position = p;
-    //     //X AND Y MULTIPLIED BY 2 FOR NOW TO EXAGGERATE MOVEMENT
-    //     camera.position.x = p.x * 2;
-    //     camera.position.y = p.y * 2 - .4; // - .4 get the head more centrally
-    //     camera.position.z = p.z + 3; //the 3 is the starting position
-    // }
-    //camera.position.x = skeleton_1.bones[0].position.x;
     requestAnimationFrame( animate );
     var delta = clock.getDelta();
     if ( mixer ) mixer.update( delta );
-    //system.update();
     renderer.render( scene, camera );
 }
 
